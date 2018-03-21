dc2cd6cdb68b02ed867a92550be1183d05ba5abb
MagnumSong <983174628@qq.com>
2018-03-21 11:16:24 +0800

    铁骑突出刀枪鸣

---
+++ WECHAT/WECHAT/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  WECHAT
+//
+//  Created by SongMenglong on 2018/3/21.
+//  Copyright © 2018年 SongMenglong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"微信第三方登录");
+    
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
