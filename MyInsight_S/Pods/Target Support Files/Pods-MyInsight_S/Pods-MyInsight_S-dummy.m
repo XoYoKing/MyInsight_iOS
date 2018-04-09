831aa9fa7427cf3ccfdd87b3c6e2fcc18034715a
MagnumSong <983174628@qq.com>
2018-04-09 14:07:25 +0800

    前月浮梁买茶去

--- MyInsight_S/Pods/Target Support Files/Pods-MyInsight_S/Pods-MyInsight_S-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MyInsight_S : NSObject
-@end
-@implementation PodsDummy_Pods_MyInsight_S
-@end
--- MyInsight_S/Pods/Target Support Files/Pods-MyInsight_SUITests/Pods-MyInsight_SUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MyInsight_SUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MyInsight_SUITests
-@end
